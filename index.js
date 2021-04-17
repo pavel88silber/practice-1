6fb7b5fe93cbe224a3f65ef1cc0494bdb506600c
Pavel Silber <silberpav@gmail.com>
2021-04-17 22:12:14 +0300

    margin float left

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const heading = document.getElementById('header')
 const par = document.getElementById('par')
 // const shape2 = document.querySelector('.shape2')
 
-addStylesTo(heading, 'black', 'red','Practice 1', '2rem', 1600)
-addStylesTo(par1, 'pink', 'blue','document это глобальный объект в браузере, доступный в Javascript', '2rem', 1800)
-addStylesTo(par2, 'green', 'white','window это глобальный объект в браузере, доступный в Javascript', '2rem', 1400)
-addStylesTo(par3, 'yellow', 'red','Эсли объект глобальный его можно опустить (alert() confirm() promt() browser API', '2rem', 1200)
-addStylesTo(par, 'red', 'white', 'setTimeout()', '2rem', 1000)
+addStylesTo(par, 'red', 'white', 'setTimeout()', '2rem', 200)
+addStylesTo(heading, 'black', 'red','Practice 1', '2rem', 600)
+addStylesTo(par1, 'pink', 'blue','document это глобальный объект в браузере, доступный в Javascript', '2rem', 800)
+addStylesTo(par2, 'green', 'white','window это глобальный объект в браузере, доступный в Javascript', '2rem', 1200)
+addStylesTo(par3, 'yellow', 'red','Эсли объект глобальный его можно опустить (alert() confirm() promt() browser API', '2rem', 1500)
+
 
 function addStylesTo (node, color, textColor, text, padd, delay) {
     setTimeout((delay)=> {
