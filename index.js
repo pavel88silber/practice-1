b33e6923bcaa0e95830c17c4b25564c68a5b5837
Pavel Silber <silberpav@gmail.com>
2021-04-17 17:41:45 +0300

    setTimeout test

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-console.log('test2')
-console.log('money ')
+const heading = document.getElementById('header')
+const par = document.getElementById('par')
+// const shape2 = document.querySelector('.shape2')
+
+addStylesTo(heading, 'black', 'red','Practice 1', '2rem', 1600)
+addStylesTo(par1, 'pink', 'blue','document это глобальный объект в браузере, доступный в Javascript', '2rem', 1800)
+addStylesTo(par2, 'green', 'white','window это глобальный объект в браузере, доступный в Javascript', '2rem', 1400)
+addStylesTo(par3, 'yellow', 'red','Эсли объект глобальный его можно опустить (alert() confirm() promt() browser API', '2rem', 1200)
+addStylesTo(par, 'red', 'white', 'setTimeout()', '2rem', 1000)
+
+function addStylesTo (node, color, textColor, text, padd, delay) {
+    setTimeout((delay)=> {
+        node.textContent = text
+        node.style.background = color
+        node.style.color = textColor
+        node.style.padding = padd
+    }, delay)
+}
+
+
