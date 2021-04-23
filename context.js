9d9d2d65d8d311012cb972a0d1a2c4669f81312b
Pavel Silber <silberpav@gmail.com>
2021-04-23 21:32:02 +0300

    Update context.js

--- context.js
+++ context.js
@@ -10,18 +10,31 @@ const person = {
         console.log('Hello  from method!')
     },
     info:() => {
-        console.log('this:', this);
-        console.info('Human name: ' + person.firstName);
+        console.log('this:', this)
+        console.log('hasOwnProperty? lastName:  ', person.hasOwnProperty('lastName')) // true
+        console.log('hasOwnProperty? info:  ', person.hasOwnProperty('info')) // true
+        console.info('Human name: ' + person.firstName)
     }
 }
 
-// person.info()
+console.log(person.info());
+console.log('--------------------------- Looger -------------------------------------')
 
-// console.log(person.firstName.blink())
-//document.getElementById('textRow3').innerHTML = person.year
 
-const logger = {
-    keys() {
-        console.log(keys);
+// Объект logger с некоторыми методами
+// const logger = {
+//     // метод keys который будет выводить в консоль ключи объекта
+//     keys: (obj) => {
+//         console.log(obj)
+//     }
+// }
+
+// Объект logger2 с некоторыми методами
+const logger2 = {
+    // метод keys который будет выводить в консоль ключи объекта теперь с this
+    keys: () => {
+        console.log(this)
     }
 }
+// С помощью this можно выводить данные любого объекта 
+console.log(logger2.keys(person)) 
