a48113684bb45086c254a2f66d1f5d7938f9e85f
Pavel Silber <34262772+silberpavel@users.noreply.github.com>
2021-04-25 14:17:04 +0300

    .call() .bind() apply()

--- context.js
+++ context.js
@@ -1,3 +1,56 @@
+const style1 = [
+    'color: powderBlue',
+    'text-shadow: 2px 2px purple', 
+    'background: plum', 
+    'font-size: 2em',
+    'border: 1px solid purple',
+    'padding: 10px',
+    'font-family: fantasy'
+].join(';');
+
+const style2 = [
+    'color: white',
+    'text-shadow: 2px 2px purple', 
+    'background: red', 
+    'font-size: 1em',
+    'border: 1px solid purple',
+    'padding: 3px'
+    // 'font-family: fantasy'
+].join(';');
+
+const style3 = [
+    'color: blue',
+    'text-shadow: 0.5px 0.5px purple', 
+    'background: plum', 
+    'font-size: 2em',
+    'border: 1px solid purple',
+    'padding: 10px',
+    'font-family: fantasy',
+    'border-radius: 2rem'
+].join(';');
+
+const styleBlack = [
+    'color: white',
+    'text-shadow: 2px 2px purple', 
+    'background: black', 
+    'font-size: 1em',
+    'border: 1px solid purple',
+    'padding: 3px'
+    // 'font-family: fantasy'
+].join(';');
+
+const style5 = [
+    'color: white',
+    'text-shadow: 2px 2px purple', 
+    'background: green', 
+    'font-size: 1em',
+    'border: 1px solid purple',
+    'padding: 3px'
+    // 'font-family: fantasy'
+].join(';');
+
+
+
 // const person = new Object()
 console.log('----------------------------------------------------------------')
 console.log('------------------------- Universal method  with (contex) ---------------------------------------')
@@ -9,15 +62,25 @@ const person = {
     greet:() => {
         console.log('Hello  from method!')
     },
-    info:() => {
-        console.log('this:', this)
+    info:(obj) => {
+        console.log('this:', obj)
         console.log('hasOwnProperty? lastName:  ', person.hasOwnProperty('lastName')) // true
         console.log('hasOwnProperty? info:  ', person.hasOwnProperty('info')) // true
         console.info('Human name: ' + person.firstName)
     }
 }
 
+const human = {
+    firstName: 'John'
+}
+
+
 console.log(person.info());
+console.log(person.info(human.firstName));  // John
+
+console.log('');
+console.log('');
+console.log('');
 
 console.log('--------------------------- Looger -------------------------------------')
 
@@ -34,8 +97,8 @@ console.log('--------------------------- Looger --------------------------------
 const logger2 = {
     num: 22,
     // метод keys который будет выводить в консоль ключи объекта теперь с this
-    keys: (obj) => {
-        console.log('Object Keys  ==>  ' + Object.keys(obj))
+    keys() {
+        console.log('Object Keys  ==>  ' + Object.keys(this))
     }
 }
 console.log(logger2.keys(person));
@@ -45,8 +108,37 @@ console.log(logger2.keys(person));
 console.log('');
 console.log('');
 console.log('');
-console.log('--------------------------Bind()------------------------------------')
-console.log('привязывает определенный кsонтекст который мы сами выберем')
-console.log('-----метод Bind() return new function ------------------------------------')
+console.log(`%c Bind() `, style3)
+console.log(`%c привязывает определенный контекст который мы сами выберем`, style2)
+console.log(`%c метод Bind() return new function`, styleBlack)
 const bound = logger2.keys.bind(person)
-bound(person)
+bound()
+console.log(`%c Call тоже самое что и Bind() но только сама себя вызывает... `, style5)
+
+logger2.keys.call(person)
+
+console.log('');
+console.log('');
+console.log('');
+console.log(`%c Практика от Владлена`, styleBlack)
+
+
+// Object.keys(person).forEach((key) => {
+//     console.log(key, person[key])
+// })
+
+const logger3 = {
+    keysAndValues() {
+        Object.keys(this).forEach((key) => {              //  Object.keys(person) ARRAY
+            console.log(key + ' * * * * * * * * * ' + this[key])
+            // console.log(`"${key}" : ${this[key]}`)
+
+        })
+    }
+}
+
+logger3.keysAndValues.call(logger3)
+// logger3.keysAndValues.call(person)
+
+const textContext = document.querySelector('#textRow3')
+textContext.innerHTML = 'Записал в тетрадь .bind() .call() apply()'
