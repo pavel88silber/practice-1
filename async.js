0d667c6c4c5f06c8ca1929a75a5793864eb3f2b1
Pavel Silber <silberpav@gmail.com>
2021-04-26 17:10:23 +0300

    New ways (Promises)

--- async.js
+++ async.js
@@ -18,22 +18,22 @@
 ///////////////////////////////////////////////////////////////////////////////////
 
 
-console.log('');
-console.log('---------------------- OLD WAY -----------------------------');
+// console.log('');
+// console.log('---------------------- OLD WAY -----------------------------');
 
-const delay = (wait = 1000) => {
-    const promise = new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve()
-        }, wait)
-    })
-    return promise
-}
+// const delay = (wait = 1000) => {
+//     const promise = new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             resolve()
+//         }, wait)
+//     })
+//     return promise
+// }
 
-delay(800)
-    .then(() => {console.log('Promise works!')})
-    .catch(err => console.log('Error: ', err))
-    .finally(() => console.log('Finally'))
+// delay(800)
+//     .then(() => {console.log('Promise works!')})
+//     .catch(err => console.log('Error: ', err))
+//     .finally(() => console.log('Finally'))
 
 ///////////////////////////////////////////////////////////////////////////////////
 
@@ -42,30 +42,53 @@ console.log('');
 console.log('');
 
 
-const delay2 = (wait = 2000) => {
+const delay2 = (wait = 1000) => {
     const promise = new Promise((resolve, reject) => {
         setTimeout(() => {
-            resolve()
+            // resolve()
+            reject('Что-то пошло не так..')
         }, wait)
     })
     return promise
 }
 
 // function return new Promise
-const getData = () => new Promise(resolve => resolve([1, 1, 2, 3, 5, 8, 13]))
+const getData = () => new Promise(resolve => resolve([
+    1, 1, 2, 3, 5, 8, 13
+]))
 
-setTimeout(() => {
-    console.log('');
-    console.log('');
-    console.log('---------------------- NEW WAY -----------------------------');
-    console.log('');
-    getData().then(data => console.log(data))
-}, 2500)
+getData().then((data => {console.log('getData().then(    ' + data);}))
 
+// setTimeout(() => {
+//     console.log('');
+//     console.log('');
+//     console.log('---------------------- NEW WAY -----------------------------');
+//     console.log('');
+//     // getData().then(data => console.log(data))
+// }, 2500)
 
+///////////////////////////////////////////////////////////////////////////////////
 
+delay2(2000)
+    .then(() => {console.log('After 2 seconds, Promise works!')})
+    .catch(err => {console.error('Error: ', err)})
+    .finally(() => {console.log('Finally!');})
 
+///////////////////////////////////////////////////////////////////////////////////
 
+async function asyncExample() {
+    try {
+        await delay2(3000)
+        const data = await getData()
+        console.log('Data: ', data);
+    } catch (e){
+        console.log(e);
+    } finally {
+        console.log('Finally');
+    }
+    
+}
 
+asyncExample()
 
 
