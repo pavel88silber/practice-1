bea617aa23de57eeeeb3148b29557819a537eb71
Pavel Silber <34262772+silberpavel@users.noreply.github.com>
2021-04-29 15:31:20 +0300

    ES6_testing

--- es6.js
+++ es6.js
@@ -1,9 +1,27 @@
-
+console.log('');
 
 const obj = {color: 'blue'}
 
-console.log(obj.color);
+// console.log(obj.color);
 
 obj.color = 'red'
 
-console.log(obj.color);
+// console.log(obj.color);
+
+const myF = () => console.log('My arrow function');
+// myF()
+
+// Context
+
+function log() {
+    console.log('function this => ' + this);
+}
+
+const arrowLogs = () => {
+    console.log('arrow function this => ' + this);
+}
+
+log()
+arrowLogs()
+
+console.log('');
